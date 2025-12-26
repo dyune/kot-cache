1ead1dc8c6a04a0ffb6a9a14257852851e9bba84
dyune <david.wang5@mail.mcgill.ca>
2025-12-25 22:49:27 -0500

    init

---
+++ src/main/kotlin/com/proj/kotcache/KotCacheApplication.kt
@@ -0,0 +1,11 @@
+package com.proj.kotcache
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotCacheApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotCacheApplication>(*args)
+}
---
+++ src/test/kotlin/com/proj/kotcache/KotCacheApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.proj.kotcache
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotCacheApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
